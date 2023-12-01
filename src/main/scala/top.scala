259f677a8975d6e8bb22b7641671dd94506e44d5
zhang <zhangzuo21@mails.tsinghua.edu.cn>
2023-12-01 19:38:06 +0800

    add top

--- src/main/scala/top.scala
+++ src/main/scala/top.scala
@@ -61,3 +61,41 @@ class TestTop extends Module {
 
 }
 
+class VivadoTop extends Module {
+  val Ifu = Module(new InstructionFetchUnit)
+    val sram0 = IO(new ExternalSramInterface)
+    val backend = Module(new Backend)
+    val decoder = Module(new DecodeUnit)
+
+    SramUtils.AddExternalSram(sram0, "baseSram")
+    val SramController = Module(new SramWithArbiter("baseSram", 4))
+
+    // 四根总线
+    val Buses = Seq.fill(4)(Module(new BusMux(1)))
+    // 将Buses的输出连接到SramController的输入
+    for(i <- 0 until 4) {
+        Buses(i).io.slaves(0) <> SramController.io.masters(i)
+    }
+    // 接入地址分配
+    for(i <- 0 until 4) {
+        Buses(i).io.allocate(0).start := BusConfig.BASE_RAM_START.U
+        Buses(i).io.allocate(0).mask := BusConfig.BASE_RAM_MASK.U
+    }
+    // 分别来自 ifu(tlb & icache), backend(storebuffer & mem_pipeline)
+    Buses(0).io.master <> Ifu.sramBusIO(0)
+    Buses(1).io.master <> Ifu.sramBusIO(1)
+    Buses(2).io.master <> backend.io.memBus(0)
+    Buses(3).io.master <> backend.io.memBus(1)
+
+    Ifu.sramBusIO(0) <> SramController.io.masters(0)
+    Ifu.sramBusIO(1) <> SramController.io.masters(1)
+    Ifu.ctrlIO.clearIcache := false.B
+    Ifu.ctrlIO.clearTLB := false.B
+    Ifu.ctrlIO.flush := backend.ctrlIO.flushPipeline
+
+    Ifu.io.redirect(0) := backend.io.robRedirect
+    Ifu.io.fetch <> decoder.io.in
+    decoder.io.out <> backend.io.in
+    decoder.io.nextDone := backend.io.renameDone
+    decoder.ctrlIO.flush := backend.ctrlIO.flushPipeline
+}
